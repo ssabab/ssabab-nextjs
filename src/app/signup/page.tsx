8a24c140c001131aea2bc81c16fcc57dae63ae58
김해리 <harry5655@naver.com>
2025-05-12 09:19:04 +0900

    feat: 캘린더 구현

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,6 +1,6 @@
 import { SignupForm } from "@/components/authentication/SignupForm"
 
-export default function Page() {
+export default function SignupPage() {
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
